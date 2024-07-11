a0bd329fbe55375e58863a3158174eddfd64a630
Vijaya Malla <mallabiju@gmail.com>
2024-07-10 22:55:13 -0700

    Control Statement

---
+++ C_Control_Statement/UsingOOP/a_if_block/a_if.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Number
+{
+    int num;
+
+public:
+    void input()
+    {
+        cout << "Enter a number:";
+        cin >> num;
+    }
+    void output()
+    {
+        if (num > 0)
+        {
+            cout << "The number " << num << " is a positive integer.";
+        }
+    }
+};
+
+int main()
+{
+    Number n;
+    n.input();
+    n.output();
+    return 0;
+}
